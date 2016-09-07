b3993341ecba46e591167b33a837623538c7b960
Colton A Palumbo <cp352@pvhs-lab112-d11.pvsd.org>
2016-09-07 14:20:52 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("What is going on?");
+	System.out.print("Hello world");
+        //The above is the two ways we need to know how to output text
+        //Two types of comment
+        //1. Line comment which is with a //
+        //2. Block comment which is with a /*
+        System.out.print(1);
+        System.out.print(1.0);
+    }
+}
